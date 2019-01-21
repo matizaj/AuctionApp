0792e3214c27fb6e685003936c645cded8d97d93
Mateusz Zajac <TRBC37@motorolasolutions.com>
2019-01-21 11:16:44 +0100

    set components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { ProductService } from './services/product.service';
+import { Product } from './model/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'auction-app';
+  products: Array<Product> = [];
+  constructor(private productService: ProductService) {
+    this.products = this.productService.getProducts();
+  }
+
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../model/product';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,72 @@ import { Injectable } from '@angular/core';
 export class ProductService {
 
   constructor() { }
+
+  getProducts(): Array<Product> {
+    return products.map(p => new Product(p.id, p.title, p.price, p.rating, p.description, p.categories));
+  }
 }
+
+const products = [
+  {
+    '_id': 0,
+    get 'id_1'() {
+      return this['_id'];
+    },
+    set 'id_1'(value) {
+      this['_id'] = value;
+    },
+    get 'id'() {
+      return this['_id'];
+    },
+    set 'id'(value) {
+      this['_id'] = value;
+    },
+    'title': 'Pierwszy produkt',
+    'price': 24.99,
+    'rating': 4.3,
+    'description': 'To jest krótki opis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    'categories': ['electronics', 'hardware']
+  },
+  {
+    'id': 1,
+    'title': 'Drugi produkt',
+    'price': 64.99,
+    'rating': 3.5,
+    'description': 'To jest krótki opis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    'categories': ['books']
+  },
+  {
+    'id': 2,
+    'title': 'Trzeci produkt',
+    'price': 74.99,
+    'rating': 4.2,
+    'description': 'To jest krótki opis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    'categories': ['electronics']
+  },
+  {
+    'id': 3,
+    'title': 'Czwarty produkt',
+    'price': 84.99,
+    'rating': 3.9,
+    'description': 'To jest krótki opis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    'categories': ['hardware']
+  },
+  {
+    'id': 4,
+    'title': 'Piąty produkt',
+    'price': 94.99,
+    'rating': 5,
+    'description': 'To jest krótki opis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    'categories': ['electronics', 'hardware']
+  },
+  {
+    'id': 5,
+    'title': 'Szósty produkt',
+    'price': 54.99,
+    'rating': 4.6,
+    'description': 'To jest krótki opis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    'categories': ['books']
+  }
+];
+
