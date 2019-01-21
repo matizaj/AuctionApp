ecfd65dfdfe8b6550ae6cf354e2bb49dfd860625
Mateusz Zajac <TRBC37@motorolasolutions.com>
2019-01-21 08:11:12 +0100

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+import { SearchComponent } from './components/search/search.component';
+import { StarsComponent } from './components/stars/stars.component';
+import { ProductService } from './services/product.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselComponent,
+    FooterComponent,
+    NavbarComponent,
+    ProductItemComponent,
+    SearchComponent,
+    StarsComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
